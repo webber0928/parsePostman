dd987feed2d4bf7a2d890cf57a51b24c6975ad31
Webber Woo <webber@webzz.co>
2020-11-24 11:10:35 +0800

    Optimize settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,65 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'test',
+    title: "test",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    "bootstrap-vue/nuxt"
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-  }
-}
+    analyze: true,
+    assetFilter: function(assetFilename) {
+      return assetFilename.endsWith(".js");
+    }
+  },
+
+  uglify: {
+    uglifyOptions: {
+      comments: false,
+      compress: {
+        drop_console: true
+      }
+    }
+  },
+
+  collapseBooleanAttributes: true,
+  collapseWhitespace: false,
+  decodeEntities: true,
+  minifyCSS: true,
+  minifyJS: true,
+  processConditionalComments: true,
+  removeAttributeQuotes: false,
+  removeComments: false,
+  removeEmptyAttributes: true,
+  removeOptionalTags: false,
+  removeRedundantAttributes: true,
+  removeScriptTypeAttributes: false,
+  removeStyleLinkTypeAttributes: false,
+  removeTagWhitespace: false,
+  sortClassName: false,
+  trimCustomFragments: true,
+  useShortDoctype: true
+};
