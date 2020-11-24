8a06ddde75be885efc8eff6c11edc3a39b104228
Webber Woo <webber@webzz.co>
2020-11-24 15:24:47 +0800

    first build docs

---
+++ docs/_nuxt/static/1606202662/parse/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/parse", {data:[{}],fetch:[],mutations:void 0});
--- nuxt.config.js
+++ nuxt.config.js
@@ -64,6 +64,7 @@ export default {
   trimCustomFragments: true,
   useShortDoctype: true,
 
-  buildDir: 'docs'
-
+  generate: {
+    dir: 'docs'
+  },
 };
